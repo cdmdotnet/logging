80eeb99cec1b25998d5faa929a435c775f1b442f
Grover Jackson <grover@cdmdotnet.com>
2017-01-23 20:52:05 +1300

    Switched to using (and referencing) cdmdotnet.Statemanagement for controlling the passing of the CorrelationId from thread to thread safely when using Parallel Tasks.

--- cdmdotnet.Logging/CorrelationIdHelper.cs
+++ cdmdotnet.Logging/CorrelationIdHelper.cs
@@ -7,35 +7,61 @@
 #endregion
 
 using System;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
+using System.Web;
+using cdmdotnet.StateManagement;
+using cdmdotnet.StateManagement.Threaded;
 
 namespace cdmdotnet.Logging
 {
 	/// <summary>
-	/// A <see cref="ICorrelationIdHelper"/> that uses the <see cref="CallContext"/> to transporting the CorrelationId within the <see cref="Thread"/>
+	/// A <see cref="ICorrelationIdHelper"/> that uses the <see cref="ThreadedContextItemCollectionFactory.GetCurrentContext"/> method to get a transport collection within the <see cref="Thread"/>.
 	/// </summary>
 	public class CorrelationIdHelper : ICorrelationIdHelper
 	{
-		private const string CallContextPermissoinScopeValueKey = "CorrelationIdValue";
-
-		#region Implementation of ICorrolationIdHelper
-
 		/// <summary>
-		/// Get the CorrelationId set via <see cref="SetCorrelationId"/> within this <see cref="Thread"/> via <see cref="CallContext.GetData"/>.
+		/// Instantiates a new instance of the <see cref="CorrelationIdHelper"/> class using the provided <paramref name="contextItemCollection"/> to set as the value of <see cref="ContextItemCollection"/>.
 		/// </summary>
-		public Guid GetCorrelationId()
+		protected CorrelationIdHelper(IContextItemCollection contextItemCollection)
 		{
-			return (Guid)CallContext.GetData(CallContextPermissoinScopeValueKey);
+			ContextItemCollection = contextItemCollection;
 		}
 
 		/// <summary>
-		/// Set the CorrelationId within this <see cref="Thread"/> via <see cref="CallContext.SetData"/> that can be read via <see cref="GetCorrelationId"/>.
+		/// Instantiates a new instance of the <see cref="CorrelationIdHelper"/> class requiring <paramref name="contextItemCollectionFactory"/>, used to set <see cref="ContextItemCollection"/> with an instance.
 		/// </summary>
-		public Guid SetCorrelationId(Guid corrolationId)
+		public CorrelationIdHelper(ThreadedContextItemCollectionFactory contextItemCollectionFactory)
 		{
-			CallContext.SetData(CallContextPermissoinScopeValueKey, corrolationId);
-			return corrolationId;
+			ContextItemCollection = contextItemCollectionFactory.GetCurrentContext();
+		}
+
+		private const string CallContextPermissionScopeValueKey = "CorrelationIdValue";
+
+		/// <summary>
+		/// The local collection that holds values.
+		/// </summary>
+		protected IContextItemCollection ContextItemCollection { get; private set; }
+
+		#region Implementation of ICorrelationIdHelper
+
+		/// <summary>
+		/// Get the CorrelationId set via <see cref="SetCorrelationId"/> 
+		/// first using <see cref="HttpContext.Items"/> if <see cref="HttpContext.Current"/> is not null
+		/// if <see cref="HttpContext.Current"/> is null OR there is a <see cref="NullReferenceException"/> then
+		/// try from, within this <see cref="Thread"/>.
+		/// </summary>
+		public Guid GetCorrelationId()
+		{
+			return ContextItemCollection.GetData<Guid>(CallContextPermissionScopeValueKey);
+		}
+
+		/// <summary>
+		/// Set the CorrelationId within this <see cref="Thread"/> that can be read via <see cref="GetCorrelationId"/>.
+		/// If <see cref="HttpContext.Current"/> is not null then also try and set it using <see cref="HttpContext.Items"/>.
+		/// </summary>
+		public Guid SetCorrelationId(Guid correlationId)
+		{
+			return ContextItemCollection.SetData(CallContextPermissionScopeValueKey, correlationId);
 		}
 
 		#endregion
--- cdmdotnet.Logging/WebCorrelationIdHelper.cs
+++ cdmdotnet.Logging/WebCorrelationIdHelper.cs
@@ -6,59 +6,22 @@
 // // -----------------------------------------------------------------------
 #endregion
 
-using System;
-using System.Runtime.Remoting.Messaging;
 using System.Threading;
-using System.Web;
+using cdmdotnet.StateManagement.Web;
 
 namespace cdmdotnet.Logging
 {
 	/// <summary>
-	/// A <see cref="ICorrelationIdHelper"/> that uses the <see cref="HttpContext.Items"/> to transporting the CorrelationId within the <see cref="Thread"/>
+	/// A <see cref="ICorrelationIdHelper"/> that uses the <see cref="WebContextItemCollectionFactory.GetCurrentContext"/> method to get a transport collection within the <see cref="Thread"/>.
 	/// </summary>
-	public class WebCorrelationIdHelper : ICorrelationIdHelper
+	public class WebCorrelationIdHelper : CorrelationIdHelper
 	{
-		private const string CallContextPermissionScopeValueKey = "CorrelationIdValue";
-
-		#region Implementation of ICorrelationIdHelper
-
 		/// <summary>
-		/// Get the CorrelationId set via <see cref="SetCorrelationId"/> 
-		/// first using <see cref="HttpContext.Items"/> if <see cref="HttpContext.Current"/> is not null
-		/// if <see cref="HttpContext.Current"/> is null OR there is a <see cref="NullReferenceException"/> then
-		/// try from, within this <see cref="Thread"/>.
+		/// Instantiates a new instance of the <see cref="WebCorrelationIdHelper"/> class requiring <paramref name="contextItemCollectionFactory"/>, used to set the internal collection with an instance.
 		/// </summary>
-		public Guid GetCorrelationId()
+		public WebCorrelationIdHelper(WebContextItemCollectionFactory contextItemCollectionFactory)
+			: base(contextItemCollectionFactory.GetCurrentContext())
 		{
-			try
-			{
-				if (HttpContext.Current != null)
-					return (Guid)HttpContext.Current.Items[CallContextPermissionScopeValueKey];
-			}
-			catch (NullReferenceException)
-			{
-			}
-			return (Guid)CallContext.GetData(CallContextPermissionScopeValueKey);
 		}
-
-		/// <summary>
-		/// Set the CorrelationId within this <see cref="Thread"/> that can be read via <see cref="GetCorrelationId"/>.
-		/// If <see cref="HttpContext.Current"/> is not null then also try and set it using <see cref="HttpContext.Items"/>.
-		/// </summary>
-		public Guid SetCorrelationId(Guid correlationId)
-		{
-			CallContext.SetData(CallContextPermissionScopeValueKey, correlationId);
-			try
-			{
-				if (HttpContext.Current != null)
-					HttpContext.Current.Items[CallContextPermissionScopeValueKey] = correlationId;
-			}
-			catch (NullReferenceException)
-			{
-			}
-			return correlationId;
-		}
-
-		#endregion
 	}
 }
