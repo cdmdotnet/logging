75d0cea8cfce556609d01bee52c6c665f3aef9a4
Grover Jackson <grover@cdmdotnet.com>
2017-03-14 17:22:39 +1300

    Added auto property population with the CorrelationId

--- cdmdotnet.Logging.Azure.ApplicationInsights/TelemetryHelper.cs
+++ cdmdotnet.Logging.Azure.ApplicationInsights/TelemetryHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
 
 namespace cdmdotnet.Logging.Azure.ApplicationInsights
@@ -13,16 +14,17 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <summary>
 		/// Instantiate a new instance of <see cref="TelemetryHelper"/>
 		/// </summary>
-		public TelemetryHelper(TelemetryClient telemetryClient)
+		public TelemetryHelper(TelemetryClient telemetryClient, ICorrelationIdHelper correlationIdHelper)
 		{
 			TelemetryClient = telemetryClient;
+			CorrelationIdHelper = correlationIdHelper;
 		}
 
 		/// <summary>
 		/// Instantiate a new instance of <see cref="TelemetryHelper"/>
 		/// </summary>
-		public TelemetryHelper()
-			:this(new TelemetryClient())
+		public TelemetryHelper(ICorrelationIdHelper correlationIdHelper)
+			:this(new TelemetryClient(), correlationIdHelper)
 		{
 			TelemetryClient.InstrumentationKey = TelemetryConfiguration.Active.InstrumentationKey;
 		}
@@ -31,6 +33,11 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// The <see cref="TelemetryClient"/> used to send telemetry data.
 		/// </summary>
 		protected TelemetryClient TelemetryClient { get; set; }
+
+		/// <summary>
+		/// Obtains the CorrelationId to stamp each telemetry operation with
+		/// </summary>
+		protected ICorrelationIdHelper CorrelationIdHelper { get; private set; }
 
 		#region Implementation of ITelemetryHelper
 
@@ -41,7 +48,9 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="properties">Named string values you can use to search and classify events.</param>
 		public void TrackEvent(string eventName, IDictionary<string, string> properties = null)
 		{
-			TelemetryClient.TrackEvent(eventName, properties);
+			IDictionary<string, string> telemetryProperties = (properties ?? new Dictionary<string, string>());
+			SetCorrelationId(telemetryProperties);
+			TelemetryClient.TrackEvent(eventName, telemetryProperties);
 		}
 
 		/// <summary>
@@ -52,7 +61,9 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="properties">Named string values you can use to search and classify events.</param>
 		public void TrackMetric(string name, double value, IDictionary<string, string> properties = null)
 		{
-			TelemetryClient.TrackMetric(name, value, properties);
+			IDictionary<string, string> telemetryProperties = (properties ?? new Dictionary<string, string>());
+			SetCorrelationId(telemetryProperties);
+			TelemetryClient.TrackMetric(name, value, telemetryProperties);
 		}
 
 		/// <summary>
@@ -63,7 +74,9 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="properties">Named string values you can use to search and classify events.</param>
 		public void TrackException(Exception exception, IDictionary<string, double> metrics = null, IDictionary<string, string> properties = null)
 		{
-			TelemetryClient.TrackException(exception, properties, metrics);
+			IDictionary<string, string> telemetryProperties = (properties ?? new Dictionary<string, string>());
+			SetCorrelationId(telemetryProperties);
+			TelemetryClient.TrackException(exception, telemetryProperties, metrics);
 		}
 
 		/// <summary>
@@ -76,6 +89,10 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="wasSuccessfull">True if the dependency call was handled successfully.</param>
 		public void TrackDependency(string dependencyName, string commandName, DateTimeOffset startTime, TimeSpan duration, bool wasSuccessfull)
 		{
+#pragma warning disable 618
+			var dependencyTelemetry = new DependencyTelemetry(dependencyName, commandName, startTime, duration, wasSuccessfull);
+#pragma warning restore 618
+			SetCorrelationId(dependencyTelemetry.Properties);
 			TelemetryClient.TrackDependency(dependencyName, commandName, startTime, duration, wasSuccessfull);
 		}
 
@@ -92,7 +109,9 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="wasSuccessfull">True if the dependency call was handled successfully.</param>
 		public void TrackDependency(string dependencyTypeName, string target, string dependencyName, string data, DateTimeOffset startTime, TimeSpan duration, string resultCode, bool wasSuccessfull)
 		{
-			TelemetryClient.TrackDependency(dependencyTypeName, target, dependencyName, data, startTime, duration, resultCode, wasSuccessfull);
+			var dependencyTelemetry = new DependencyTelemetry(dependencyTypeName, target, dependencyName, data, startTime, duration, resultCode, wasSuccessfull);
+			SetCorrelationId(dependencyTelemetry.Properties);
+			TelemetryClient.TrackDependency(dependencyTelemetry);
 		}
 
 		/// <summary>
@@ -105,9 +124,23 @@ namespace cdmdotnet.Logging.Azure.ApplicationInsights
 		/// <param name="wasSuccessfull">True if the request was handled successfully by the application.</param>
 		public void TrackRequest(string name, DateTimeOffset startTime, TimeSpan duration, string responseCode, bool wasSuccessfull)
 		{
-			TelemetryClient.TrackRequest(name, startTime, duration, responseCode, wasSuccessfull);
+			var requestTelemetry = new RequestTelemetry(name, startTime, duration, responseCode, wasSuccessfull);
+			SetCorrelationId(requestTelemetry.Properties);
+			TelemetryClient.TrackRequest(requestTelemetry);
 		}
 
 		#endregion
+
+		/// <summary>
+		/// Sets the CorrelationId into the provided <paramref name="properties"/>
+		/// </summary>
+		protected virtual void SetCorrelationId(IDictionary<string, string> properties)
+		{
+			try
+			{
+				properties["CorrelationId"] = CorrelationIdHelper.GetCorrelationId().ToString("N");
+			}
+			catch { }
+		}
 	}
 }
