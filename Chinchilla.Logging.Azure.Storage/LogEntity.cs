2ba892e4770021d6d1d1884c32053853fd4842f6
Grover Jackson <grover@cdmdotnet.com>
2020-12-18 19:37:23 +1300

    Added Azure Storage.

---
+++ Chinchilla.Logging.Azure.Storage/LogEntity.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Chinchilla.Logging.Azure.Storage
+{
+	/// <summary>
+	/// Information about an event to be logged
+	/// </summary>
+	public class LogEntity : TableEntity
+	{
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="LogEntity"/>.
+		/// </summary>
+		public LogEntity(string level, string container)
+		{
+			PartitionKey = level;
+			RowKey = container;
+		}
+
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="LogEntity"/>.
+		/// </summary>
+		public LogEntity(LogInformation logInformation)
+			: this (logInformation.Level, logInformation.Container)
+		{
+			Raised = logInformation.Raised;
+			Level = logInformation.Level;
+			Message = logInformation.Message;
+			Container = logInformation.Container;
+			Exception = logInformation.Exception;
+			MetaData = logInformation.MetaData;
+			CorrolationId = logInformation.CorrolationId;
+		}
+
+		/// <summary>
+		/// The <see cref="DateTime"/> the event was raised.
+		/// </summary>
+		public DateTime Raised { get; set; }
+
+		/// <summary>
+		/// The level of the event, such as 'error', 'info' or 'debug'
+		/// </summary>
+		public string Level { get; set; }
+
+		/// <summary>
+		/// A Human readable message describing the event
+		/// </summary>
+		public string Message { get; set; }
+
+		/// <summary>
+		/// The container such as a method or class the event was raised from
+		/// </summary>
+		public string Container { get; set; }
+
+		/// <summary>
+		/// A serialised <see cref="Exception"/> if one was provided.
+		/// </summary>
+		public string Exception { get; set; }
+
+		/// <summary>
+		/// A serialised <see cref="IDictionary{TKey,TValue}"/> if one was provided.
+		/// </summary>
+		public string MetaData { get; set; }
+
+		/// <summary>
+		/// The value from <see cref="ICorrelationIdHelper.GetCorrelationId"/>
+		/// </summary>
+		public Guid CorrolationId { get; set; }
+
+		/// <summary />
+		public string Module { get; set; }
+
+		/// <summary />
+		public string Instance { get; set; }
+
+		/// <summary />
+		public string Environment { get; set; }
+
+		/// <summary />
+		public string EnvironmentInstance { get; set; }
+	}
+}
---
+++ Chinchilla.Logging.Azure.Storage/TableStorageLogger.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using Chinchilla.Logging.Configuration;
+using Microsoft.Azure.Cosmos.Table;
+
+#if NETSTANDARD2_0
+#else
+#endif
+
+namespace Chinchilla.Logging.Azure.Storage
+{
+	/// <summary>
+	/// Provides a set of methods that help you log events relating to the execution of your code outputting to an Azure Table Storage account
+	/// </summary>
+	public abstract class TableStorageLogger<TEntity>
+		: Logger
+		where TEntity : LogEntity
+	{
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="TableStorageLogger{TEntity}"/> class calling the constructor on <see cref="Logger"/>.
+		/// </summary>
+		protected TableStorageLogger(ILoggerSettings loggerSettings, ICorrelationIdHelper correlationIdHelper, ITelemetryHelper telemetryHelper)
+			: base(loggerSettings, correlationIdHelper, telemetryHelper)
+		{
+		}
+
+		/// <summary />
+		protected virtual string GetConnectionString(string container)
+		{
+			string appSettingValue = GetSetting(container, containerLoggerSettings => containerLoggerSettings.SqlDatabaseLogsConnectionStringName, LoggerSettings.SqlDatabaseLogsConnectionStringName);
+			if (string.IsNullOrWhiteSpace(appSettingValue))
+				throw new ConfigurationErrorsException("No value for the setting 'SqlDatabaseLogsConnectionStringName' was provided");
+			string connectionString = LoggerSettings.GetConnectionString(appSettingValue);
+			return connectionString;
+		}
+
+		/// <summary>
+		/// Converts the provided <paramref name="logInformation"/> into a <typeparamref name="TEntity"/>
+		/// </summary>
+		protected abstract TEntity ConvertLogInformation(LogInformation logInformation);
+
+		/// <summary>
+		/// Persists (or saves) the provided <paramref name="logInformation"></paramref> to the database
+		/// </summary>
+		/// <param name="logInformation">The <see cref="LogInformation"/> holding all the information you want to persist (save) to the database.</param>
+		protected override void PersistLog(LogInformation logInformation)
+		{
+			try
+			{
+				string tableName = GetSetting(logInformation.Container, containerLoggerSettings => containerLoggerSettings.SqlDatabaseTableName, LoggerSettings.SqlDatabaseTableName);
+				string connectionString = GetConnectionString(logInformation.Container);
+
+				CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+				CloudTableClient tblclient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+				CloudTable table = tblclient.GetTableReference(tableName);
+				table.CreateIfNotExists();
+
+				TEntity logEntity = ConvertLogInformation(logInformation);
+				logEntity.Module = LoggerSettings.ModuleName;
+				logEntity.Instance = LoggerSettings.Instance;
+				logEntity.Environment = LoggerSettings.Environment;
+				logEntity.EnvironmentInstance = LoggerSettings.EnvironmentInstance;
+
+				TableOperation insertOperation = TableOperation.Insert(logEntity);
+				table.Execute(insertOperation);
+			}
+			catch (Exception exception)
+			{
+				Trace.TraceError("Persisting log failed with the following exception:\r\n{0}\r\n{1}", exception.Message, exception.StackTrace);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Provides a set of methods that help you log events relating to the execution of your code outputting to an Azure Table Storage account
+	/// </summary>
+	public class TableStorageLogger
+		: TableStorageLogger<LogEntity>
+	{
+		/// <summary>
+		/// Instantiates a new instance of the <see cref="TableStorageLogger{TEntity}"/> class calling the constructor on <see cref="Logger"/>.
+		/// </summary>
+		protected TableStorageLogger(ILoggerSettings loggerSettings, ICorrelationIdHelper correlationIdHelper, ITelemetryHelper telemetryHelper)
+			: base(loggerSettings, correlationIdHelper, telemetryHelper)
+		{
+		}
+
+		/// <summary>
+		/// Converts the provided <paramref name="logInformation"/> into a <see cref="LogEntity"/>
+		/// </summary>
+		protected override LogEntity ConvertLogInformation(LogInformation logInformation)
+		{
+			return new LogEntity(logInformation);
+		}
+	}
+}
